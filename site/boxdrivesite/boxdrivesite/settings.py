07b5389a15208f3ae371ac5b068c727dea76b1f2
abuly <abulyaev@yan.ru>
2021-02-20 02:19:42 +0300

    Add some styles, htmls etc

--- site/boxdrivesite/boxdrivesite/settings.py
+++ site/boxdrivesite/boxdrivesite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'boxdriveapp.apps.BoxdriveappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
