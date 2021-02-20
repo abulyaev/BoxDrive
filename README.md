cb3b889af5520caffd84f6c81661658122211ae3
abuly <abulyaev@yan.ru>
2021-02-20 19:51:59 +0300

    set up Admin panel, some test model and views

--- README.md
+++ README.md
@@ -11,4 +11,17 @@ To run django server:
 python.exe manage.py runserver
 ```
 
+To apply changes to DB models:
+```
+python.exe manage.py makemigrations
+```
+
+```
+python.exe manage.py sqlmigrate boxdriveapp <number from migrations folder>
+```
+
+```
+python.exe manage.py migrate
+```
 How to create a pull request to this repository: https://opensource.com/article/19/7/create-pull-request-github
+
--- site/boxdrivesite/boxdriveapp/models.py
+++ site/boxdrivesite/boxdriveapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# TODO: Here we need to discuss about fields
+class Post(models.Model):
+    objects = models.Manager()
+    fileName = models.CharField(max_length=128)
+    fileUrl = models.TextField()
+    datePosted = models.DateTimeField(default=timezone.now)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.fileName
+
