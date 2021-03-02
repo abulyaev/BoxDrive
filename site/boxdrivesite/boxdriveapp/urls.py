6c99f8cbb6d79ba08baf7470993c5c9c1fff0af9
abuly <abulyaev@yan.ru>
2021-03-03 02:42:32 +0300

    Individual pages for files

--- site/boxdrivesite/boxdriveapp/urls.py
+++ site/boxdrivesite/boxdriveapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='boxdrive-home'),
+    path('', PostListView.as_view(), name='boxdrive-home'),
+    path('file/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # path('', views.home, name='boxdrive-home'),
     path('about/', views.about, name='boxdrive-about'),
 ]
