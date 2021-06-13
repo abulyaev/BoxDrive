0466b229dda6835ade024aa07d37eda196a13e33
Kirill Shcherbinin <shcherbinin.kir.norm.acc@gmail.com>
2021-06-13 19:40:28 +0300

    new document views

--- site/boxdrivesite/boxdriveapp/views.py
+++ site/boxdrivesite/boxdriveapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import (
     ListView,
     DetailView,
@@ -6,7 +6,9 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-from . models import Post
+from . models import Post, Document
+from boxdriveusersreg.models import Profile
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
@@ -16,6 +18,59 @@ def home(request):
         'files': Post.objects.all()
     }
     return render(request, 'boxdriveapp/home.html', context)
+
+
+def about(request):
+    return render(request, 'boxdriveapp/about.html')
+
+##################################################################
+class DocumentListView(ListView):
+    model = Document
+    template_name = 'boxdriveapp/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'files'
+    #ordering = ['pk']
+
+
+class DocumentDetailView(DetailView):
+    model = Document
+
+
+class DocumentCreateView(LoginRequiredMixin, CreateView):
+    model = Document
+    fields = ['title', 'file_field']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class DocumentUploadView(ListView):
+    def get(self, request, username):
+        return render(request, 'main/upload_file.html')
+
+
+    def post(self, request, username):
+        filename = request.FILES['filename']
+        title = request.POST['title']
+
+        user_obj = Profile.objects.get(username=username)
+        upload_doc = Document(user=user_obj, title=title, file_field=filename)
+        upload_doc.save()
+        messages.success(request, 'Your Post has been uploaded successfully.')
+        return render(request, 'main/upload_file.html')
+
+
+class DocumentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Document
+
+    def get(self, request, document_id):
+        user = request.session['user']
+        delete_doc = self.model.objects.get(id=document_id)
+        delete_doc.delete()
+        messages.success(request, 'Your post has been deleted successfully.')
+        return redirect(f'')
+
+#################################################################
 
 
 class PostListView(ListView):
@@ -63,6 +118,3 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
-
-def about(request):
-    return render(request, 'boxdriveapp/about.html')
