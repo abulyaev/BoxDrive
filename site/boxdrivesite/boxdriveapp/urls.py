a4836b69647eb07ee15d258d7539cea888937fe2
Kirill Shcherbinin <shcherbinin.kir.norm.acc@gmail.com>
2021-06-13 19:53:30 +0300

    update urls

--- site/boxdrivesite/boxdriveapp/urls.py
+++ site/boxdrivesite/boxdriveapp/urls.py
@@ -10,10 +10,19 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='boxdrive-home'),
-    path('file/new/', PostCreateView.as_view(), name='post-create'),
-    path('file/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('file/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('file/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    # path('', views.home, name='boxdrive-home'),
     path('about/', views.about, name='boxdrive-about'),
+
+    path('file/new/', PostCreateView.as_view(), name='post-create'),
+    #path('file/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('file/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    #path('file/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    # path('', views.home, name='boxdrive-home'),
+
+    #############################################
+    path('<str:username>/upload_file/', views.DocumentUploadView.as_view(), name='upload_file'),
+    path('delete/<int:document_id>/', views.DocumentDeleteView.as_view(), name='post-delete'),
+
+    #path('file/new/', PostCreateView.as_view(), name='post-create'),
+    path('file/<int:pk>/', views.DocumentDetailView.as_view(), name='post-detail'),
+    #path('file/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 ]
