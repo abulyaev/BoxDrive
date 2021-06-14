f9e325e96fd7565394bf473f47f36f5a859609ab
Kirill Shcherbinin <shcherbinin.kir.norm.acc@gmail.com>
2021-06-14 16:35:56 +0300

    show ALL files on home page

--- site/boxdrivesite/boxdriveapp/urls.py
+++ site/boxdrivesite/boxdriveapp/urls.py
@@ -4,21 +4,24 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
-)
+    PostDeleteView)
 from . import views
 
+
 urlpatterns = [
-    path('', PostListView.as_view(), name='boxdrive-home'),
-    path('about/', views.about, name='boxdrive-about'),
+    #path('', PostListView.as_view(), name='boxdrive-home'),
+    #path('about/', views.about, name='boxdrive-about'),
 
     #path('file/new/', PostCreateView.as_view(), name='post-create'),
     #path('file/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('file/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    #path('file/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     #path('file/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     # path('', views.home, name='boxdrive-home'),
 
     #############################################
+    path('', views.DocumentListView.as_view(), name='boxdrive-home'),
+    #path('', views.get_doc_list, name='boxdrive-home'),
+    path('about/', views.about, name='boxdrive-about'),
     path('file/<str:cur_user>/upload_file/', views.DocumentUploadView.as_view(), name='upload_file'),
     path('delete/<int:document_id>/', views.DocumentDeleteView.as_view(), name='post-delete'),
     path('file/<int:pk>/', views.DocumentDetailView.as_view(), name='post-detail'),
--- site/boxdrivesite/boxdriveapp/views.py
+++ site/boxdrivesite/boxdriveapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.views.generic import (
     ListView,
     DetailView,
@@ -7,8 +7,12 @@ from django.views.generic import (
     DeleteView
 )
 from . models import Post, Document
+from django.contrib.auth.models import User
 from boxdriveusersreg.models import Profile
 from django.contrib import messages
+from rest_framework import filters
+from rest_framework import generics
+#from boxdriveapp.serializers import UserSerializer
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
@@ -23,12 +27,52 @@ def home(request):
 def about(request):
     return render(request, 'boxdriveapp/about.html')
 
+
 ##################################################################
+def get_doc_list(request, *args, **kwargs):
+    user_doc_list = Document.objects.filter(cur_user=Document.cur_user)
+
+    context = {
+        'doc-list': user_doc_list,
+    }
+
+    return render(request, 'boxdriveapp/home.html', context)
+
+
 class DocumentListView(ListView):
     model = Document
-    template_name = 'boxdriveapp/home.html'  # <app>/<model>_<viewtype>.html
-    context_object_name = 'files'
-    #ordering = ['pk']
+    template_name = 'boxdriveapp/home.html'
+    context_object_name = 'documents'
+
+    '''
+    # функция для фильтрации документов по юзеру 
+    def get_queryset(self, *args, **kwargs):
+        return Document.objects.filter(cur_user=self.request.user)
+    '''
+
+    '''
+    def get_queryset(self, *args, **kwargs):
+        self.cur_user = get_object_or_404(Profile, user=self.kwargs['cur_user'])
+        #currentuser = self.model.objects.get(cur_user=self.kwargs['cur_user'])
+        return Document.objects.filter(cur_user=self.cur_user)
+        #return currentuser
+    '''
+
+
+
+
+class HomePageView(ListView):
+    model = Document
+    #context_object_name = 'files'
+
+    def get_doc_list(self, request, cur_user, *args, **kwargs):
+        user_doc_list = Document.objects.filter(created_by=cur_user)
+
+        context = {
+            'doc-list': user_doc_list,
+        }
+
+        return render(request, 'boxdriveapp/home.html', context)
 
 
 class DocumentDetailView(DetailView):
@@ -44,7 +88,7 @@ class DocumentCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class DocumentUploadView(ListView):
+class DocumentUploadView(UpdateView):
     def get(self, request, cur_user, *args, **kwargs):
         return render(request, 'boxdriveapp/upload_file.html', {'cur_user': cur_user,})
 
